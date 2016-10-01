61d50113834e7d7bad395a0b8b0bf80ae15300aa
Harry Moore <harrymoore@yahoo.com>
2016-10-01 15:52:30 -0400

    create titlemanager module

---
+++ ui-extensions/titlemanager/index.js
@@ -0,0 +1,12 @@
+define(function(require) {
+
+    // page: "titlemanger-products-list"
+    require("./gadgets/titlemanger-products-list/titlemanger-products-list.js");
+
+    // dashlet: "titlemanger-notifications-dashlet"
+    require("./gadgets/titlemanger-notifications-dashlet/titlemanger-notifications-dashlet.js");
+
+    // global CSS overrides
+    require("css!./styles/titlemanger.css");
+
+});
