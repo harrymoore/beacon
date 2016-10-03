90fe9d71d7e3b03c8de20d7aa842585adee17f68
Harry Moore <harrymoore@yahoo.com>
2016-10-03 09:34:07 -0400

    titlemanager module updates

--- ui-extensions/titlemanager/gadgets/titlemanager-notifications-dashlet/titlemanager-notifications-dashlet.js
+++ ui-extensions/titlemanager/gadgets/titlemanager-notifications-dashlet/titlemanager-notifications-dashlet.js
@@ -1,13 +1,18 @@
 define(function(require, exports, module) {
 
-    var html = require("text!./titlemanager-notifications-dashlet.html");
     require("css!./titlemanager-notifications-dashlet.css");
 
-    var UI = require("ui");
+    var Ratchet = require("ratchet/web");
+    var OneTeam = require("oneteam");
+    var Bundle = Ratchet.Messages.using();
+    var List = require("ratchet/dynamic/list");
 
-    return UI.registerDashlet("titlemanager-notifications-dashlet", UI.AbstractDashlet.extend({
+    return Ratchet.DashletRegistry.register("project-tasks", List.extend({
 
-        TEMPLATE: html,
+        setup: function()
+        {
+            this.get(this.index);
+        },
 
         configureDefault: function()
         {
@@ -16,14 +21,11 @@ define(function(require, exports, module) {
 
             // now add in our custom configuration
             this.config({
-                "chrome": true,
-                "columnHeaders": true,
+                "chrome": false,
+                "columnHeaders": false,
                 "columns": [{
-                    "title": "Project",
-                    "key": "project"
-                },{
-                    "title": "Task Description",
-                    "key": "taskDescription"
+                    "title": "Task",
+                    "key": "titleDescription"
                 }],
                 "options": {
                     "filter": false,
@@ -31,142 +33,100 @@ define(function(require, exports, module) {
                     "info": false,
                     "sizing": false,
                     "processing": false,
-                    "zeroRecords": "No content items were found."
+                    "zeroRecords": "No tasks were found."
                 },
                 "icon": true,
-                "loader": "remote"
+                "loader": "gitana"
             });
         },
 
-        columnValue: function(row, item, model)
+        entityTypes: function()
+        {
+            return {
+                "plural": "tasks",
+                "singular": "task"
+            }
+        },
+
+        prepareModel: function(el, model, callback)
+        {
+            callback();
+        },
+
+        doGitanaQuery: function(context, model, searchTerm, query, pagination, callback)
         {
             var self = this;
 
+            var project = self.observable("project").get();
+
+            // bring back the last 5 projects
+            OneTeam.platform(function() {
+
+                var query = {};
+
+                if (project)
+                {
+                    query["context.projectId"] = project.getId();
+                }
+
+                pagination = {
+                    "limit": 5,
+                    "sort": {
+                        "_system.modified_on.ms": -1
+                    }
+                };
+
+                var filter = "assigned";
+
+                this.queryTasksForCurrentUser(filter, query, pagination).then(function() {
+                    callback(this);
+                });
+
+            });
+        },
+
+        linkUri: function(row, model, context)
+        {
+            return OneTeam.linkUri(this, row);
+        },
+
+        iconClass: function(row)
+        {
+            return "workflow-task-icon-48";
+        },
+
+        columnValue: function(row, item, model, context)
+        {
             var value = this.base(row, item);
 
-            if (item.key == "project")
-            {
-                var title = row.title;
-                if (!title) {
-                    title = row._doc;
+            if (item.key == "titleDescription") {
+                value = OneTeam.listTitleDescription(context, row, null, null, true);
+
+                if (row.workflowTitle)
+                {
+                    value += "<p class='list-row-info description'>Workflow: " + OneTeam.filterXss(row.workflowTitle) + "</p>";
                 }
-
-                var contentUri = self.linkUri(row, model);
-
-                // title
-                value =  "<h2 class='list-row-info title'>";
-                value += "<a href='" + contentUri + "'>";
-                value += OneTeam.filterXss(title);
+            }
+            else if (item.key == "project") {
+                if (row.context && row.context.projectTitle)
+                {
+                    value = "<a href='#/projects/" + row.context.projectId + "'>";
+                    value += OneTeam.filterXss(row.context.projectTitle);
+                    value += "</a>";
+                }
+            }
+            else if (item.key == "assignedTo") {
+                value = "<a href='#/members/" + row.assigneePrincipalId + "'>";
+                value += OneTeam.filterXss(row.assigneePrincipalName);
                 value += "</a>";
-                value += "</h2>";
-
-                // description
-                if (row.description)
-                {
-                    value += "<p class='list-row-info description'>" + OneTeam.filterXss(row.description) + "</p>";
-                }
-
-                /*
-                // project
-                var projectUri = "#/projects/" + row["_projectId"];
-                value += "<p class='list-row-info description'>";
-                value += "Located in project: ";
-                value += "<a href='" + projectUri + "'>";
-                value += row._projectTitle;
-                value += "</a>";
-                value += "</p>";
-                */
-
-                if (row._system.created_on)
-                {
-                    var date = new Date(row._system.created_on.ms);
-                    value += "<p class='list-row-info created'>Created " + Bundle.relativeDate(date);
-                    if (row._system.created_by) {
-                        value += " by " + row._system.created_by;
-                    }
-                    value += "</p>";
-                }
-                else if (row._system.modified_on)
-                {
-                    var date = new Date(row._system.modified_on.ms);
-                    value += "<p class='list-row-info modified'>Modified " + Bundle.relativeDate(date);
-                    if (row._system.modified_by) {
-                        value += " by " + row._system.modified_by;
-                    }
-                    value += "</p>";
-                }
-
-                //var date = new Date(row._system.created_on.ms);
-                //var dateString = Bundle.relativeDate(date);
-            } else if (item.key == "taskDescription") {
-                var date = new Date();
-                value += "<p class='list-row-info created'>Due " + Bundle.relativeDate(date);
-                value += "</p>";
+                value += "<p class='list-row-info description'>" + row.timeStarted.timestamp + "</p>";
             }
 
             return value;
         },
 
         /**
-         * Puts variables into the model for rendering within our template.
-         * Once we've finished setting up the model, we must fire callback().
-         *
-         * @param el
-         * @param model
-         * @param callback
-         */
-        prepareModel: function(el, model, callback) {
-
-            // get the current project
-            // var project = this.observable("project").get();
-
-            // the current branch
-            var branch = this.observable("branch").get();
-
-            // call into base method and then set up the model
-            this.base(el, model, function() {
-
-                // query for catalog:product instances
-                branch.queryNodes({ "_type": "beacon:schedule" }).then(function() {
-
-                    // all of the products
-                    var products = this.asArray();
-                    for(var i = 0; i < products.length; i++) {
-                        var product = products[i];
-
-                        product.imageUrl = "/preview/repository/" + product.getRepositoryId() + "/branch/" + product.getBranchId() + "/node/" + product.getId() + "/default?size=64&name=preview64";
-                    }
-
-                    model.products = products;
-                    
-                    callback();
-                });
-            });
-        },
-
-        /**
-         * This method gets called before the rendered DOM element is injected into the page.
-         *
-         * @param el the dom element
-         * @param model the model used to render the template
-         * @param callback
-         */
-        /*
-        beforeSwap: function(el, model, callback)
-        {
-            this.base(el, model, function() {
-                callback();
-            });
-        },
-        */
-
-        /**
-         * This method gets called after the rendered DOM element has been injected into the page.
-         *
-         * @param el the new dom element (in page)
-         * @param model the model used to render the template
-         * @param originalContext the dispatch context used to inject
-         * @param callback
+         * @override
          */
         afterSwap: function(el, model, context, callback)
         {
@@ -174,12 +134,10 @@ define(function(require, exports, module) {
 
             this.base(el, model, context, function() {
 
-                // TODO: grab any injected DOM elements and bind JS behaviors if needed
-
                 callback();
-
             });
         }
-        
+
     }));
+
 });
