26e7d7b05b42d90f5d9ee28e337740cf7768c018
Harry Moore <harrymoore@yahoo.com>
2016-10-05 12:56:06 -0400

    titlemanager module updates

--- ui-extensions/titlemanager/index.js
+++ ui-extensions/titlemanager/index.js
@@ -2,7 +2,7 @@ define(function(require) {
 
     // page: "titlemanager-products-list"
     require("./gadgets/titlemanager-products-list/titlemanager-products-list.js");
-    require("./gadgets/titlemanager-products-list/products-filter.js");
+    require("./gadgets/titlemanager-products-list/titlemanager-products-filter.js");
 
     // dashlet: "titlemanager-notifications-dashlet"
     // require("./gadgets/titlemanager-notifications-dashlet/titlemanager-notifications-dashlet.js");
