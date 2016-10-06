0d5b9aaf9646a5745658c1d849d954bd19b4bf4a
Harry Moore <harrymoore@yahoo.com>
2016-10-06 13:27:56 -0400

    module updates

--- ui-extensions/test1/index.js
+++ ui-extensions/test1/index.js
@@ -1,12 +1,12 @@
 define(function(require) {
 
-    require("./actions/view-in-kaltura");
-    require("./fields/kaltura-video-picker-field");
-    require("./gadgets/project/kaltura/videos/kaltura-videos-list");
-    require("./gadgets/project/manage/kaltura/manage-kaltura");
-    require("./pickers/kaltura-video-picker");
+    require("./actions/view-in-kaltura.js");
+    require("./fields/kaltura-video-picker-field.js");
+    require("./gadgets/project/kaltura/videos/kaltura-videos-list.js");
+    require("./gadgets/project/manage/kaltura/manage-kaltura.js");
+    require("./pickers/kaltura-video-picker.js");
 
     require("css!./styles/kaltura.css");
 
-    require ("./engine");
+    require ("./engine.js");
 });
